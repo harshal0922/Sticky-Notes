70477dd16c1250d30731df3572fb31738fb58f8c
harshal0922 <harshal2209@gmail.com>
2021-11-02 16:26:02 +0530

    Sticky Notes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import notes from './notes'
+import Note from "./components/Note.jsx";
+
+
+    ReactDOM.render(<App />,document.getElementById('root'));
+    
